48e6ba232e87a09ed9a901e480bdd734e923521a
233191 <233191@students.aih.nsw.edu.au>
2024-08-21 17:47:30 +1000

    Update GraphPlot.py

--- GraphPlot.py
+++ GraphPlot.py
@@ -1,2 +1,4 @@
 #Learning Visualisation using Python Libraries (Pandas, Matplotlib and Seaborn)
 import pandas as pd
+df = pd.DataFrame(np.random.randn(10,4), index=pd.date_range('1/1/2000',periods=10),  columns=list('ABCD'))
+
